f174b9f8daf0f9df0dd1ee2972518cc2961adc52
Matt <mpschirle1@gmail.com>
2023-01-30 19:08:10 -0800

    Corrected typo

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import { mockData } from "./mock-data";
 import axios from "axios";
-import { NProgress } from "nprogress";
+import NProgress from "nprogress";
 
 const removeQuery = () => {
   if (window.history.pushState && window.location.pathname) {
