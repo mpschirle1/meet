37ba5d00a54486ec8ad1ef6392cc6f6d04d36c75
Matt <mpschirle1@gmail.com>
2023-02-16 20:39:38 -0800

    Added offline warning message

--- src/EventList.js
+++ src/EventList.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import Event from "./Event";
+import { WarningAlert } from "./Alert";
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
+    const online = navigator.onLine;
     return (
-      <ul className="EventList">
-        {events.map((event) => (
-          <li key={event.id}>
-            <Event event={event} />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <WarningAlert text="Warning: You are currently offline. Only previously viewed events may be displayed." />
+        <ul className="EventList">
+          {events.map((event) => (
+            <li key={event.id}>
+              <Event event={event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
